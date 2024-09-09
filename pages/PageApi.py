5c87ca0a5743b3859f40bf6c973a39cc7f05ea04
farit777 <farit777@mail.ru>
2024-09-09 11:12:16 +0300

    Доработал PageApi.py и test_api.py

--- pages/PageApi.py
+++ pages/PageApi.py
@@ -12,38 +12,38 @@ class PageApi:
         """
         Функция выполняет запрос к API для отправки товара в корзину.
         Возвращаемое значение:
-        dict: Словарь, содержащий данные о товаре отправленном в корзину.
+        - Функция возвращает status_code запроса
         """
         with allure.step("Запрос на отправку товара в корзину"):
-            path = ("{base_url}/ajax/ajax_ecommerce/ajax_ecommerce".format(base_url=self.base_url))
-            resp = requests.post(path, headers=new_headers, payload=new_body)
+            path = ("{base_url}/cart/add_products_to_cart_from_preview.php".format(base_url=self.base_url))
+            resp = requests.post(path, headers=new_headers, data=new_body)
         with allure.step("Возврат инфо о товаре, добавленного в корзину"):
-            return  resp.json()
+            return  resp.status_code
 
-    # Получить список стран
-    @allure.title("API: Добаление количества товара в корзине")
-    def add_product_quantity(self, field_name):
+    # Добавить количествотовара в корзине
+    @allure.title("API: Добавление количества товара в корзине")
+    def add_product_quantity(self, new_headers: dict, new_body: dict):
         """
         Функция выполняет запрос к API для добаления количества товара в корзине.
         Возвращаемое значение
-        - Функция возвращает ответ API в формате JSON, который содержит количество добавленного товара
+        - Функция возвращает status_code запроса
         """
         with allure.step("Запрос на добаление количества товара в корзине"):
-            path = ("{base_url}/movie/possible-values-by-field?field={f_name}".format(base_url=self.base_url, f_name=field_name))
-            resp = requests.post(path, headers=self.headers)
+            path = ("{base_url}/cart/add_products_to_cart_from_preview.php".format(base_url=self.base_url))
+            resp = requests.post(path, headers=new_headers, data=new_body)
         with allure.step("Возврат количества добавленного товара"):
-            return  resp.json()
+            return  resp.status_code
     
     # Получить список жанров
     @allure.title("API: Удаление товара из корзины")
-    def remove_product_from_cart(self, field_name):
+    def remove_product_from_cart(self, new_headers: dict, new_body: dict):
         """
         Функция выполняет запрос к API для удаления товара из корзины.
         Возвращаемое значение
-        - `dict`: Функция возвращает ответ API в формате JSON, который содержит информацию об удаленном товре.
+        - Функция возвращает status_code запроса
         """
         with allure.step("Запрос на Удаление товара из корзины"):
-            path = ("{base_url}/movie/possible-values-by-field?field={f_name}".format(base_url=self.base_url, f_name=field_name))
-            resp = requests.post(path, headers=self.headers)
+            path = ("{base_url}/cart/delete_products_from_cart_preview.php".format(base_url=self.base_url))
+            resp = requests.post(path, headers=new_headers, data=new_body)
         with allure.step("Возврат инфо об удаленном товаре"):    
-            return  resp.json()
+            return  resp.status_code
--- tests/test_api.py
+++ tests/test_api.py
@@ -11,9 +11,9 @@ api = PageApi()
 def test_add_product_to_cart():
     """
     Функция предназначена для тестирования API, который отправляет товар в корзину.
-    Тест проверяет, что ...  .
+    - Функция возвращает status_code запроса
     """
-    with allure.step("Получить список список фильмов по названию"):
+    with allure.step("Отправляем товар в корзину"):
         myheaders = {
             'accept': '*/*',
             'accept-encoding': 'gzip, deflate, br, zstd',
@@ -24,7 +24,7 @@ def test_add_product_to_cart():
             'cookie': 'CID=2039c84b964e51cb378657bd6f5d5cda; site_countryID=247'
         }
 
-        payload1 = {
+        payload = {
             'product_id': 1781,
             'LANG_key': 'ru',
             'S_wh': 1,
@@ -34,15 +34,71 @@ def test_add_product_to_cart():
             'quantity': 1
         }
 
-        payload2 = {
-            'action': 'ecom_link_products_cart',
-            'productID': 1781,
+        status_code = api.add_product_to_cart(myheaders, payload)
+    with allure.step("Проверить, что список не пустой"):
+        assert status_code == 200
+
+@allure.suite("API: Тестирование добавления количества товара в корзине")
+@allure.title("API: Добавить количество товара в корзине")
+def test_add_product_quantity():
+    """
+    Функция предназначена для тестирования API, добавляет уоличество товара в корзине.
+    - Функция возвращает status_code запроса
+    """
+    with allure.step("Добавляем количество товара в корзине"):
+        myheaders = {
+            'accept': '*/*',
+            'accept-encoding': 'gzip, deflate, br, zstd',
+            'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+            'connection': 'keep-alive',
+            'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'cookie': 'CID=2039c84b964e51cb378657bd6f5d5cda; site_countryID=247'
+        }
+
+        payload = {
+            'product_id': 1781,
             'LANG_key': 'ru',
             'S_wh': 1,
             'S_CID': '2039c84b964e51cb378657bd6f5d5cda',
             'S_cur_code': 'rub',
-            'S_koef': 1
+            'S_koef': 1,
+            'quantity': 1,
+            'S_hint_code': '',
+            'S_customerID': ''
         }
-        result = api.add_product_to_cart(myheaders, payload2)
+
+        status_code = api.add_product_quantity(myheaders, payload)
     with allure.step("Проверить, что список не пустой"):
-        assert len(result) > 0
+        assert status_code == 200
+
+@allure.suite("API: Тестирование удаления товара из корзины")
+@allure.title("API: Удаление товара из корзины")
+def test_remove_product_from_cart():
+    """
+    Функция предназначена для тестирования API, удаляет ьовар из корзины.
+    - Функция возвращает status_code запроса
+    """
+    with allure.step("Удаляем товар из корзине"):
+        myheaders = {
+            'accept': '*/*',
+            'accept-encoding': 'gzip, deflate, br, zstd',
+            'accept-language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+            'connection': 'keep-alive',
+            'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+            'cookie': 'CID=2039c84b964e51cb378657bd6f5d5cda; site_countryID=247'
+        }
+
+        payload = {
+            'product_id': 1781,
+            'LANG_key': 'ru',
+            'S_wh': 1,
+            'S_CID': '2039c84b964e51cb378657bd6f5d5cda',
+            'S_cur_code': 'rub',
+            'S_koef': 1,
+            'S_hint_code': '',
+            'S_customerID': ''
+        }
+
+        status_code = api.remove_product_from_cart(myheaders, payload)
+    with allure.step("Проверить, что список не пустой"):
+        assert status_code == 200
